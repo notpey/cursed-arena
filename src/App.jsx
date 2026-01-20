228d8b982c6836fb2c791b82595151330cc3797a
notpey <breedlove.peyton@gmail.com>
2026-01-20 11:27:51 -0500

    Initial commit - project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="game-container">
+      <h1>⚔️ CURSED ARENA ⚔️</h1>
+      <p>A Tactical 3v3 Battle Game</p>
+      <p style={{ color: '#888', marginTop: '20px' }}>
+        If you're seeing this, your project is working babe! 💅
+      </p>
+    </div>
+  )
+}
+
+export default App
